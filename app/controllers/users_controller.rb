768a490151068c82a1d940ff00ebeeba1321ac3d
PaulusU <paulus.siswanto@ukirama.com>
2024-02-28 10:35:34 +0700

    Fix Delete User as Admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,8 +62,4 @@ class UsersController < ApplicationController
       redirect_to(root_url, status: :see_other) unless current_user.admin?
     end
 
-    def admin_user
-      redirect_to(root_url, status: :see_other) unless current_user.admin?
-    end
-
 end
